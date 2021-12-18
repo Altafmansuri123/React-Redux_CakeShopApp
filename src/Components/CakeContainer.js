4c6ec05e553e50c02cbcae44991f1efeda1d7dd8
Altafmansuri123 <altafmansuri780@gmail.com>
2021-12-18 09:21:25 +0530

    Action reducer and store created

---
+++ src/Components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer() {
+    return (
+        <div>
+            <h1>Number of Cakes</h1>
+            <button>Buy Cakes</button>
+        </div>
+    )
+}
+
+export default CakeContainer
